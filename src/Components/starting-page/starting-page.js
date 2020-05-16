4b44913df57dd7c610ce3ba15c0813af96db7350
bombila78 <bombila78@gmail.com>
2020-05-16 19:39:53 +0300

    added social networks buttons and registration page

--- src/Components/starting-page/starting-page.js
+++ src/Components/starting-page/starting-page.js
@@ -1,17 +1,12 @@
 import React from "react";
-import TextField from "@material-ui/core/TextField";
-import Avatar from "@material-ui/core/Avatar";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import WeekendIcon from '@material-ui/icons/Weekend';
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import Link from "@material-ui/core/Link";
 import {makeStyles} from "@material-ui/core/styles";
+import Authorisation from "../authorisation/authorisation";
+import Registration from "../registration/registration";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
-const startingPageStyles = makeStyles((theme) => ({
+
+const authorisationStyles = makeStyles((theme) => ({
     root: {
         height: '100vh',
         display: 'flex',
@@ -24,92 +19,20 @@ const startingPageStyles = makeStyles((theme) => ({
         backgroundPosition: 'center',
         height: '100vh',
     },
-    paper: {
-        margin: theme.spacing(8, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    avatar: {
-        margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.light,
-    },
-    form: {
-        width: '100%',
-        marginTop: theme.spacing(1)
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2)
-    }
-
 }));
 
 const StartingPage = () => {
-    const classes = startingPageStyles();
+    const classes = authorisationStyles();
 
     return (
-        <Grid component="main" className={classes.root}>
-            <Grid item xs={false} sm={4} md={7} className={classes.image}/>
-            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-                <div className={classes.paper}>
-                    <Avatar className={classes.avatar}>
-                        <WeekendIcon/>
-                    </Avatar>
-                    <Typography component="h1" variant="h5">
-                        Авторизация
-                    </Typography>
-                    <form className={classes.form} noValidate>
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            variant="outlined"
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="password"
-                            label="Password"
-                            type="password"
-                            id="password"
-                            autoComplete="current-password"
-                        />
-                        <FormControlLabel
-                            control={<Checkbox value="remember" color="primary"/>}
-                            label="Запомнить меня"
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="primary"
-                            className={classes.submit}
-                        >
-                            Войти
-                        </Button>
-                        <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Забыли пароль?
-                                </Link>
-                            </Grid>
-                            <Grid item>
-                                <Link href="#" variant="body2">
-                                    Еще не зарегистрированы? Зарегистрироваться.
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </form>
-                </div>
+        <Router>
+            <Grid component="main" className={classes.root}>
+                <Grid item xs={false} sm={4} md={7} className={classes.image}/>
+                <Route path="/" component={Authorisation} exact />
+                <Route path="/registration" component={Registration} />
             </Grid>
-        </Grid>
+        </Router>
+
     )
 }
 
